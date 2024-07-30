31ff0857b506439fde8cd02c2d4e6af929e76d9c
Mmakgosana <makgakammakgosana@gmail.com>
2024-07-30 15:38:20 +0200

    "Updated ProductCard component: added export statements for properties, implemented handleClick and addToFavourites functions, and refactored HTML structure with added comments."

--- src/components/Products/ProductCard.svelte
+++ src/components/Products/ProductCard.svelte
@@ -3,6 +3,7 @@
   import Ratings from './Ratings.svelte';
   //import { navigate } from 'svelte-routing';
 
+  //Exporting properties that can be passed from a parent component
   export let id;
   export let title;
   export let image;
@@ -10,15 +11,17 @@
   export let rating;
   export let category;
 
+  //Function to handle click events, navigating to the product details page
   function handleClick() {
     navigate(`/products/${id}`);
   }
-
+  //Function to handle adding a product to favorites
   function addToFavourites(event) {
+    //Prevents click event from propagating to parent elements
     event.stopPropagation();
   }
 </script>
-
+<!--Main container for the product card-->
 <div
   on:click={handleClick}
   class="flex flex-col max-h-[130rem] cursor-pointer max-w-80 hover:-translate-y-1 hover:scale-105 duration-300 bg-white border border-slate-200 shadow shadow-slate-950/5 rounded-2xl overflow-hidden"
@@ -34,18 +37,24 @@
           </div>
         </h2>
       </header>
+
+      <!--Ratings component displaying product rating-->
       <Ratings {rating} />
       <div class="text-base line-clamp-2 font-extrabold text-slate-500 leading-snug">
         <h2>${price}</h2>
       </div>
     </div>
 
+    <!--Container for category and action buttons-->
+
     <div class="flex mt-1 space-x-2">
+      <!--Product category-->
       <div class="justify-start flex-1">
         <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
           {category}
         </span>
       </div>
+      <!--Container for action buttons-->
       <div class="justify-end space-x-2">
         <button on:click={addToFavourites}>
           <svg
@@ -67,7 +76,7 @@
             />
           </svg>
         </button>
-
+        <!--Add to cart button-->
         <button class="inline-flex justify-center whitespace-nowrap rounded-lg bg-cyan-700 px-3 py-2 text-sm font-medium text-white hover:bg-cyan-900 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors">
           Add To Cart
         </button>
