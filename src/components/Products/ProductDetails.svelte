940fb51fef1fe0c11c70d5444b0a47c06c61918e
Mmakgosana <makgakammakgosana@gmail.com>
2024-07-30 09:34:43 +0200

    Introduce product data, filtering, sorting and loading states

--- src/components/Products/ProductDetails.svelte
+++ src/components/Products/ProductDetails.svelte
@@ -3,3 +3,13 @@
   import { writable } from 'svelte/store';
 </script>
 
+let products = writable([]);
+  let originalProducts = [];
+  let selectedProduct = null;
+  let categories = ["All categories"];
+  let searchTerm = "";
+  let filterItem = writable("All categories");
+  let sorting = writable("default");
+  let loading = writable(false);
+  let error = writable(false);
+
